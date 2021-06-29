cf1009d1151b4d2b8073c78a88ae284ace360a0b
Samuel Noble <samuel.noble@patagonia-e.com>
2021-06-29 15:58:29 -0300

    Fixed things based on cretification documentation

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 3000
 var app = express();
 
 mercadopago.configure({
-    access_token: 'TEST-8471938166952833-062003-ca49401116b09bc46b618aee7e3c050f-778348314'
+    access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'//'TEST-8471938166952833-062003-ca49401116b09bc46b618aee7e3c050f-778348314'
 });
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded({
@@ -31,17 +31,22 @@ app.get('/detail', function (req, res) {
 
 app.get('/success', function (req, res) {
     console.dir(req.query);
-    res.render('home');
+    var pageInfo = {
+        payment_method_id: req.query.payment_method_id,
+        external_reference: req.query.external_reference,
+        collection_id: req.query.collection_id || req.query.payment_id
+    };
+    res.render('success', pageInfo);
 });
 
 app.get('/pending', function (req, res) {
     console.dir(req.query);
-    res.render('home');
+    res.render('pending');
 });
 
 app.get('/failure', function (req, res) {
     console.dir(req.query);
-    res.render('home');
+    res.render('failure');
 });
 
 app.post('/notification', function (req, res) {
@@ -51,12 +56,16 @@ app.post('/notification', function (req, res) {
 
 app.post('/buy-product', function (req, res) {
     var item = req.body;
+    item.id = '123';
+    item.description = 'Dispositivo móvil de Tienda e-commerce';
     item.currency_id = 'UYU';
     item.quantity = parseFloat(item.quantity);
     item.unit_price = parseFloat(item.unit_price);
+    item.external_reference = 'enoble2009@gmail.com';
     console.dir(item);
 
     var preference = {
+        integrator_id: "dev_24c65fb163bf11ea96500242ac130004",
         items: [
             item
         ],
@@ -66,13 +75,36 @@ app.post('/buy-product', function (req, res) {
             pending: req.headers.host + '/pending',
             failure: req.headers.host + '/failure'
         },
-        auto_return: "approved"
+        auto_return: "approved",
+        payment_methods: {
+            "excluded_payment_methods": [
+                {id: 'amex'}
+            ],
+            "excluded_payment_types": [
+                {id: 'atm'}
+            ],
+            installments: 6
+        },
+        payer: {
+            name: 'Lalo',
+            surname: 'Landa',
+            email: 'test_user_63274575@testuser.com',
+            phone: {
+                area_code: '11',
+                number: '22223333'
+            },
+            address: {
+                street_name: 'Falsa',
+                street_number: 123,
+                zip_code: '1111'
+            }
+        }
     };
 
     mercadopago.preferences.create(preference)
         .then((msg) => {
             console.dir(msg);
-            res.redirect(msg.body.sandbox_init_point);
+            res.redirect(msg.body.init_point);
         });
 });
 
